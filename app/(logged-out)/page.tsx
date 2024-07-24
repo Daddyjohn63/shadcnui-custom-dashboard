f0011e86bbb86b0ca4763180e64c3f574ce9e972
Daddyjohn63 <john@webandprosper.co.uk>
2024-07-24 15:56:05 +0100

    first commit

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button';
+import { PersonStandingIcon } from 'lucide-react';
+import Link from 'next/link';
+
+export default function LandingPage() {
+  return (
+    <>
+      <h1 className="flex gap-2 items-center">
+        <PersonStandingIcon size={50} className="text-pink-500" />
+        Team Support
+      </h1>
+      <p>Your dashboard to manage customer support</p>
+      <div className="flex gap-2 items-center">
+        <Button asChild>
+          <Link href="/login">Log in</Link>
+        </Button>
+        <small>or</small>
+        <Button asChild variant="outline">
+          <Link href="/sign-up">Sign up</Link>
+        </Button>
+      </div>
+    </>
+  );
+}
